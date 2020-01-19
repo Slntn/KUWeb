60d20ea600c5fd6955dea7533dfcab91f4bc79b3
slntn <slntn@yahoo.com>
2020-01-19 11:25:04 -0500

    Form - in progress

--- upload.php
+++ upload.php
@@ -16,13 +16,23 @@ if( !isset($_POST['fname']) ||
     !isset($_POST['email']) ||
     !isset($_POST['phone']) ||
     !isset($_POST['msg']) ||
-    !isset($_POST['files']) 
+    !isset($_FILES['files']) 
 ){ 
     died('We are sorry, but there appears to be a problem with the form you submitted.');
 }
 
 $message = "Name: $fromLName,  $fromName . Message: $msg  \n\n Phone: $phone Email: $email"; 
 $message .= "Uploads: ".clean_string($uploads)."\n";
+
+$boundary = md5("random"); // define boundary with a md5 hashed value 
+
+//attachment 
+$message .= "--$boundary\r\n"; 
+$message .="Content-Type: $file_type; name=".$file_name."\r\n"; 
+$message .="Content-Disposition: attachment; filename=".$file_name."\r\n"; 
+$message .="Content-Transfer-Encoding: base64\r\n"; 
+$message .="X-Attachment-Id: ".rand(1000, 99999)."\r\n\r\n";  
+$message .= $encoded_content; // Attaching the encoded file with email 
 
 // EMAIL HEADERS
 @mail($to, $subject, $message); 
