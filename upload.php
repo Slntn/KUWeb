634ab88fb6ae1503e0d6956adc710e8418cc2383
Drosha <drosha@gmail.com>
2020-01-19 20:16:54 +0300

    Send images by email

--- upload.php
+++ upload.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once './phpmailer/phpmailer.php';
+
 $to = "info@kitchen-update.com";
 $subject = "Form Request";
 
@@ -21,21 +23,36 @@ if( !isset($_POST['fname']) ||
     died('We are sorry, but there appears to be a problem with the form you submitted.');
 }
 
-$message = "Name: $fromLName,  $fromName . Message: $msg  \n\n Phone: $phone Email: $email"; 
-$message .= "Uploads: ".clean_string($uploads)."\n";
+$message = "Name: $fromLName,  $fromName . Message: $msg  \r\n Phone: $phone Email: $email";
 
-$boundary = md5("random"); // define boundary with a md5 hashed value 
+$mail = new PHPMailer;
+$mail->AddAddress($to);
+$mail->MsgHTML($message);
+$mail->Subject = $subject;
+$mail->From = $email;
 
-//attachment 
-$message .= "--$boundary\r\n"; 
-$message .="Content-Type: $file_type; name=".$file_name."\r\n"; 
-$message .="Content-Disposition: attachment; filename=".$file_name."\r\n"; 
-$message .="Content-Transfer-Encoding: base64\r\n"; 
-$message .="X-Attachment-Id: ".rand(1000, 99999)."\r\n\r\n";  
-$message .= $encoded_content; // Attaching the encoded file with email 
+$attachment = [];
+foreach ($uploads['error'] as $key => $error) {
+    if ($error == 0) {
+        if (move_uploaded_file($uploads['tmp_name'][$key], dirname(__FILE__) . '/tmp_images/' . $uploads['name'][$key])) {
+            $attachment[] = dirname(__FILE__) . '/tmp_images/' . $uploads['name'][$key];
+            $mail->AddAttachment(dirname(__FILE__) . '/tmp_images/' . $uploads['name'][$key]);
+        }
+    }
+}
+
+if ($mail->Send()) {
+    foreach ($attachment as $item) {
+        unlink($item);
+    }
+}
+
+//attachment
+
+
 
 // EMAIL HEADERS
-@mail($to, $subject, $message); 
+//@mail($to, $subject, $message);
 
 echo '<script language="javascript">';
 echo 'alert("Thank you. We will contact you shortly.")';
